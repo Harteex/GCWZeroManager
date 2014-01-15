f5ce07d7cb5536f66a0e97260cad46d9a62bdc2f
Harteex <harteex@gmail.com>
2014-01-15 00:22:15 +0000

    * Remove download from context menu for now in manage applications as it's not implemented * Update licenses

--- GCWZeroManager/GCWZeroManager/WindowLicenses.xaml.cs
+++ GCWZeroManager/GCWZeroManager/WindowLicenses.xaml.cs
@@ -10,6 +10,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Resources;
 
 namespace GCWZeroManager
 {
@@ -18,48 +20,25 @@ namespace GCWZeroManager
     /// </summary>
     public partial class WindowLicenses : Window
     {
-        private string[] licenseText = { "Copyright (c) 2013 Andreas Bjerkeholt" +
-                                           "\n\n" + 
-                                           "Permission is hereby granted, free of charge, to any person obtaining a copy" +
-                                           " of this software and associated documentation files (the \"Software\"), to deal" +
-                                           " in the Software without restriction, including without limitation the rights" +
-                                           " to use, copy, modify, merge, publish, distribute, sublicense, and/or sell" +
-                                           " copies of the Software, and to permit persons to whom the Software is" +
-                                           " furnished to do so, subject to the following conditions:" +
-                                           "\n\n" +
-                                           "The above copyright notice and this permission notice shall be included in" +
-                                           " all copies or substantial portions of the Software." +
-                                           "\n\n" +
-                                           "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR" +
-                                           " IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY," +
-                                           " FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE" +
-                                           " AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER" +
-                                           " LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM," +
-                                           " OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN" +
-                                           " THE SOFTWARE.",
-                                       "License: Creative Commons Attribution-NoDerivs 3.0\n" +
-                                           "Attribution: Icon Design by Creative Freedom (http://www.creativefreedom.co.uk/icon-design/)\n" +
-                                           "All copyright for Shimmer Icons belongs to Creative Freedom Ltd.\n" +
-                                           "\n" +
-                                           "http://creativecommons.org/licenses/by-nd/3.0/",
-                                       "License: Creative Commons Attribution 3.0\n" +
-                                           "Attribution: Ray Cheung - WebAppers.com\n" +
-                                           "\n" +
-                                           "You are free:\n" +
-                                           "- to use the icons for your personal or commercial projects in any way you like \n" +
-                                           "- to share it with others (but please keep the Terms Of Use.txt file intact together with the icons.) \n" +
-                                           "\n" +
-                                           "You cannot:\n" +
-                                           "- claim ownership of the icons (copyrights belong exclusively to Ray Cheung - WebAppers.com)\n" +
-                                           "- sell them to third parties\n" +
-                                           "- bundle them and offer them toghether with your products as a free gift\n" +
-                                           "\n" +
-                                           "http://creativecommons.org/licenses/by/3.0/"
-                                       };
+        private string[] licenseText = new string[2];
         public WindowLicenses()
         {
             InitializeComponent();
+
+            licenseText[0] = GetLicenseText("License.txt");
+            licenseText[1] = GetLicenseText("License-Oxygen-Icons.txt");
+
             textBox1.Text = licenseText[comboBox1.SelectedIndex];
+        }
+
+        private string GetLicenseText(string file)
+        {
+            Uri uri = new Uri(String.Format("pack://application:,,,/{0}", file));
+            StreamResourceInfo stream = System.Windows.Application.GetResourceStream(uri);
+            StreamReader sr = new StreamReader(stream.Stream);
+            var content = sr.ReadToEnd();
+
+            return content;
         }
 
         private void comboBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
